e4073f4b8f5c0aabd2dc7eff0cac6a51aee3304b
Eugene Cheipesh <echeipesh@gmail.com>
2017-05-12 14:57:21 -0400

    GeoTrellisDataSource

---
+++ src/main/scala/org/apache/spark/sql/gt/sources/GeoTrellisDataSource.scala
@@ -0,0 +1,26 @@
+package org.apache.spark.sql.gt.sources
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.sources._
+import java.net.URI
+
+import geotrellis.spark._
+import geotrellis.vector.Extent
+
+class GeoTrellisDataSource extends DataSourceRegister with RelationProvider {
+  def shortName(): String = "geotrellis"
+
+  def createRelation(sqlContext: SQLContext, parameters: Map[String, String]): BaseRelation = {
+    require(parameters.contains("uri"), "'uri' parameter for catalog location required.")
+    require(parameters.contains("layer"), "'layer' parameter for raster layer name required.")
+    require(parameters.contains("zoom"), "'zoom' parameter for raster layer zoom level required.")
+
+    val uri: URI = URI.create(parameters("uri"))
+    val layerId: LayerId = LayerId(parameters("layer"), parameters("zoom").toInt)
+    val bbox: Option[Extent] = parameters.get("bbox").map(Extent.fromString)
+
+    // here would be the place to read the layer metadata
+    // and dispatch based on key type to spatial or spacetime relation
+    GeoTrellisRelation(sqlContext, uri, layerId, bbox)
+  }
+}
---
+++ src/main/scala/org/apache/spark/sql/gt/sources/GeoTrellisRelation.scala
@@ -0,0 +1,78 @@
+package org.apache.spark.sql.gt.sources
+
+import java.net.URI
+
+import geotrellis.raster.Tile
+import geotrellis.spark.io._
+import geotrellis.spark._
+import geotrellis.spark.io.file.FileLayerReader
+import geotrellis.spark.io.hadoop.HadoopLayerReader
+import geotrellis.util.LazyLogging
+import geotrellis.vector.Extent
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext, gt}
+import org.apache.spark.sql.gt.types.{ExtentUDT, TileUDT}
+import org.apache.spark.sql.sources.{BaseRelation, Filter, PrunedFilteredScan}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+
+
+case class GeoTrellisRelation(sqlContext: SQLContext, uri: URI, layerId: LayerId, bbox: Option[Extent])
+  extends BaseRelation with PrunedFilteredScan with LazyLogging {
+  gt.gtRegister(sqlContext)
+
+  // TODO: implement sizeInBytes
+
+  override def schema: StructType = StructType(List(
+    StructField("col", DataTypes.IntegerType, nullable =false),
+    StructField("row", DataTypes.IntegerType, nullable =false),
+    StructField("extent", ExtentUDT, nullable =true),
+    StructField("tile", TileUDT, nullable =true)
+  ))
+
+  def buildScan(requiredColumns: Array[String]): RDD[Row] = {
+    buildScan(requiredColumns, Array.empty[Filter])
+  }
+
+  override def unhandledFilters(filters: Array[Filter]): Array[Filter] = {
+    // only primitive comparisons can be pushed down, not UDFs
+    // @see DataSourceStrategy.scala:509 translateFilter()
+    filters
+  }
+
+  override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
+    logger.debug(s"Reading: $layerId from $uri")
+    logger.debug(s"Required columns: ${requiredColumns.toList}")
+    logger.debug(s"PushedDown filters: ${filters.toList}")
+
+    implicit val sc = sqlContext.sparkContext
+
+    // TODO: check they type of layer before reading, generating time column dynamically
+    val reader = GeoTrellisRelation.layerReaderFromUri(uri)
+    val query = reader.query[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](layerId)
+    val rdd = bbox match {
+      case Some(extent) => query.where(Intersects(extent)).result
+      case None => query.result
+    }
+
+    rdd.map { case (sk: SpatialKey, tile: Tile) =>
+      val key_extent: Extent = rdd.metadata.layout.mapTransform(sk)
+      Row(sk.col, sk.row, key_extent, tile)
+    }
+  }
+}
+
+object GeoTrellisRelation {
+  def layerReaderFromUri(uri: URI)(implicit sc: SparkContext): FilteringLayerReader[LayerId] = {
+    uri.getScheme match {
+      case "file" =>
+        FileLayerReader(uri.getSchemeSpecificPart)
+
+      case "hdfs" =>
+        val path = new org.apache.hadoop.fs.Path(uri)
+        HadoopLayerReader(path)
+
+      // others require modules outside of geotrellis-spark
+    }
+  }
+}
