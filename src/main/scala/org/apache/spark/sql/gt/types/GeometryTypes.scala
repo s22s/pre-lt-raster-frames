eccdfb45cd193db0b58b1b87e97e27eee9d09c6b
Simeon H.K. fitch <fitch@astraea.io>
2017-05-12 09:01:13 -0400

    Fixed copy/paste error in GeometryTypes inheritance.

--- src/main/scala/org/apache/spark/sql/gt/types/GeometryTypes.scala
+++ src/main/scala/org/apache/spark/sql/gt/types/GeometryTypes.scala
@@ -8,14 +8,14 @@ import geotrellis.vector._
  * @author sfitch 
  * @since 5/11/17
  */
-private[gt] class PointUDT extends AbstractTileUDT[Point]("st_point")
+private[gt] class PointUDT extends AbstractGeometryUDT[Point]("st_point")
 case object PointUDT extends PointUDT
 
-private[gt] class LineUDT extends AbstractTileUDT[Line]("st_line")
+private[gt] class LineUDT extends AbstractGeometryUDT[Line]("st_line")
 case object LineUDT extends LineUDT
 
-private[gt] class MultiLineUDT extends AbstractTileUDT[MultiLine]("st_multiline")
+private[gt] class MultiLineUDT extends AbstractGeometryUDT[MultiLine]("st_multiline")
 case object MultiLineUDT extends MultiLineUDT
 
-private[gt] class PolygonUDT extends AbstractTileUDT[Polygon]("st_polygon")
+private[gt] class PolygonUDT extends AbstractGeometryUDT[Polygon]("st_polygon")
 case object PolygonUDT extends PolygonUDT
