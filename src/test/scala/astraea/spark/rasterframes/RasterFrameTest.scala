e45ca086081739aceb4596d61c3820b73c866d18
Simeon H.K. fitch <fitch@astraea.io>
2017-08-29 16:57:31 -0400

    Incremental progress toward fixing tile layer clipping before rasterization.

--- src/test/scala/astraea/spark/rasterframes/RasterFrameTest.scala
+++ src/test/scala/astraea/spark/rasterframes/RasterFrameTest.scala
@@ -10,7 +10,7 @@ import geotrellis.spark._
 import geotrellis.spark.io._
 import geotrellis.spark.testkit.TileLayerRDDBuilders
 import geotrellis.spark.tiling._
-import geotrellis.vector.ProjectedExtent
+import geotrellis.vector.{Extent, ProjectedExtent}
 import org.apache.spark.sql.functions._
 
 /**
@@ -26,18 +26,15 @@ class RasterFrameTest extends TestEnvironment with TestData with LazyLogging {
   describe("RasterFrame") {
     it("should implicitly convert from layer type") {
 
-      val tile = randomTile(20, 20, "uint8")
-
-      val tileLayerRDD: TileLayerRDD[SpatialKey] =
-        TileLayerRDDBuilders.createTileLayerRDD(tile, 2, 2, LatLng)._2
+      val tileLayerRDD = TestData.randomTileLayerRDD(20, 20, 2, 2)
 
       val rf = tileLayerRDD.toRF
 
       assert(rf.tileColumns.nonEmpty)
       assert(rf.spatialKeyColumn.toString() == "spatial_key")
 
-      rf.printSchema()
-      rf.orderBy("spatial_key").show(false)
+//      rf.printSchema()
+//      rf.orderBy("spatial_key").show(false)
 
       assert(rf.schema.head.metadata.contains(CONTEXT_METADATA_KEY))
       assert(rf.schema.head.metadata.json.contains("tileLayout"))
@@ -86,8 +83,51 @@ class RasterFrameTest extends TestEnvironment with TestData with LazyLogging {
     it("should provide TileLayerMetadata") {
       val rf = sampleGeoTiff.projectedRaster.toRF(256, 256)
       val tlm = rf.tileLayerMetadata
+      rf.select(rf.spatialKeyColumn).show(100)
       assert(tlm.bounds.get._1 === SpatialKey(0, 0))
-      assert(tlm.bounds.get._2 === SpatialKey(4, 2))
+      assert(tlm.bounds.get._2 === SpatialKey(3, 1))
+    }
+
+    it("should clip TileLayerMetadata extent") {
+      val tiled = sampleTileLayerRDD
+
+      val rf = tiled.toRF
+
+      val worldish = Extent(-179, -89, 179, 89)
+      val areaish = Extent(-90, 30, -81, 39)
+
+      val orig = tiled.metadata.extent
+      assert(orig.contains(worldish))
+      assert(orig.contains(areaish))
+
+      val clipped = rf.clipLayerExtent.tileLayerMetadata.extent
+      assert(!clipped.contains(worldish))
+      assert(clipped.contains(areaish))
+    }
+
+    def basicallySame(expected: Extent, computed: Extent): Unit = {
+      val components = Seq(
+        (expected.xmin, computed.xmin),
+        (expected.ymin, computed.ymin),
+        (expected.xmax, computed.xmax),
+        (expected.ymax, computed.ymax)
+      )
+      forEvery(components)(c ⇒
+        assert(c._1 === c._2 +- 0.000001)
+      )
+    }
+
+    it("shouldn't clip already clipped extents") {
+      val rf = TestData.randomTileLayerRDD(1024, 1024, 8, 8).toRF
+
+      val expected = rf.tileLayerMetadata.extent
+      val computed = rf.clipLayerExtent.tileLayerMetadata.extent
+      basicallySame(expected, computed)
+
+      val rf2 = sampleGeoTiff.projectedRaster.toRF(256, 256)
+      val expected2 = rf2.tileLayerMetadata.extent
+      val computed2 = rf2.clipLayerExtent.tileLayerMetadata.extent
+      basicallySame(expected2, computed2)
     }
 
     def Greyscale(stops: Int): ColorRamp = {
@@ -140,6 +180,7 @@ class RasterFrameTest extends TestEnvironment with TestData with LazyLogging {
       val equalized = rf.select(equalizer($"tile") as "equalized")
 
       intercept[IllegalArgumentException] {
+        // spatial_key is lost
         equalized.asRF.toRaster($"equalized", 128, 128)
       }
     }
