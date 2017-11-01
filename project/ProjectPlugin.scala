147dbcd395b8e1c2032a92c336bcfe41454db879
Simeon H.K. Fitch <fitch@astraea.io>
2017-11-01 15:41:25 -0400

    Ported to sbt 1.0.2. Had to abandon git-flow support via sbt-release derivative.

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -4,28 +4,20 @@ import sbtrelease.ReleasePlugin.autoImport.ReleaseTransformations._
 import sbtrelease.ReleasePlugin.autoImport._
 
 import _root_.bintray.BintrayPlugin.autoImport._
-import com.servicerocket.sbt.release.git.flow.Steps._
 import com.typesafe.sbt.SbtGit.git
 import com.typesafe.sbt.sbtghpages.GhpagesPlugin
 import com.typesafe.sbt.site.SitePlugin.autoImport._
 import com.typesafe.sbt.site.paradox.ParadoxSitePlugin.autoImport._
-import de.heikoseeberger.sbtheader.CommentStyleMapping
-import de.heikoseeberger.sbtheader.HeaderKey.headers
-import de.heikoseeberger.sbtheader.license.Apache2_0
-import tut.TutPlugin
+//import com.typesafe.sbt.site.paradox.ParadoxSitePlugin.autoImport._
 import tut.TutPlugin.autoImport._
 import GhpagesPlugin.autoImport._
 import com.lightbend.paradox.sbt.ParadoxPlugin.autoImport._
-import com.typesafe.sbt.site.SiteScaladocPlugin
-import com.typesafe.sbt.site.paradox.ParadoxSitePlugin
 
 /**
  * @author sfitch
  * @since 8/20/17
  */
 object ProjectPlugin extends AutoPlugin {
-  override def requires = SiteScaladocPlugin && ParadoxSitePlugin && TutPlugin && GhpagesPlugin
-
   override def trigger: PluginTrigger = allRequirements
 
   val versions = Map(
@@ -40,13 +32,12 @@ object ProjectPlugin extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "io.astraea",
-    organizationName := "Astraea",
+    organizationName := "Astraea, Inc.",
+    startYear := Some(2017),
     homepage := Some(url("http://rasterframes.io")),
     scmInfo := Some(ScmInfo(url("https://github.com/s22s/raster-frames"), "git@github.com:s22s/raster-frames.git")),
     description := "RasterFrames brings the power of Spark DataFrames to geospatial raster data, empowered by the map algebra and tile layer operations of GeoTrellis",
-    startYear := Some(2017),
     licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
-    headers := CommentStyleMapping.createFrom(Apache2_0, "2017", "Astraea, Inc."),
     scalaVersion := "2.11.11",
     scalacOptions ++= Seq("-feature", "-deprecation"),
     cancelable in Global := true,
@@ -96,21 +87,23 @@ object ProjectPlugin extends AutoPlugin {
         bintrayOrganization := Some("s22s"),
         bintrayReleaseOnPublish in ThisBuild := false,
         publishArtifact in (Compile, packageDoc) := false,
+        releaseIgnoreUntrackedFiles := true,
+        releaseTagName := s"${version.value}",
         releaseProcess := Seq[ReleaseStep](
           checkSnapshotDependencies,
-          checkGitFlowExists,
           inquireVersions,
+          runClean,
           runTest,
-          gitFlowReleaseStart,
           setReleaseVersion,
           buildSite,
           publishSite,
           commitReleaseVersion,
+          tagRelease,
           publishArtifacts,
           releaseArtifacts,
-          gitFlowReleaseFinish,
           setNextVersion,
-          commitNextVersion
+          commitNextVersion,
+          pushChanges
         )
       )
     }
@@ -118,10 +111,11 @@ object ProjectPlugin extends AutoPlugin {
     def docSettings: Seq[Def.Setting[_]] = Seq(
       git.remoteRepo := "git@github.com:s22s/raster-frames.git",
       apiURL := Some(url("http://rasterframes.io/latest/api")),
+      autoAPIMappings := false,
       paradoxProperties in Paradox ++= Map(
-        "github.base_url" -> "https://github.com/s22s/raster-frames",
-        "scaladoc.org.apache.spark.sql.gt" -> "http://rasterframes.io/latest",
-        "scaladoc.geotrellis.base_url" -> "https://geotrellis.github.io/scaladocs/latest"
+        "github.base_url" -> "https://github.com/s22s/raster-frames"//,
+        //"scaladoc.org.apache.spark.sql.gt" -> "http://rasterframes.io/latest" //,
+        //"scaladoc.geotrellis.base_url" -> "https://geotrellis.github.io/scaladocs/latest"
       ),
       sourceDirectory in Paradox := tutTargetDirectory.value,
       sourceDirectory in Paradox in paradoxTheme := sourceDirectory.value / "main" / "paradox" / "_template",
@@ -138,8 +132,8 @@ object ProjectPlugin extends AutoPlugin {
       ),
       scalacOptions in (Compile,doc) += "-J-Xmx6G",
       // NB: These don't seem to work. Still trying to figure Tut's run model.
-      fork in (Tut, run) := true,
-      javaOptions in (Tut, run) += "-Xmx6G"
+      fork in (Tut, runner) := true,
+      javaOptions in (Tut, runner) += "-Xmx6G"
     )
   }
 }
