c2502c0bdc3e0aa272dd5e212aee10ee37f1e0c3
Simeon H.K. Fitch <fitch@astraea.io>
2017-11-01 15:41:25 -0400

    Ported to sbt 1.0.2. Had to abandon git-flow support via sbt-release derivative.

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -14,15 +14,11 @@ import com.typesafe.sbt.site.paradox.ParadoxSitePlugin.autoImport._
 import tut.TutPlugin.autoImport._
 import GhpagesPlugin.autoImport._
 import com.lightbend.paradox.sbt.ParadoxPlugin.autoImport._
-import com.typesafe.sbt.site.SiteScaladocPlugin
-import com.typesafe.sbt.site.paradox.ParadoxSitePlugin
 
 /**
  * @since 8/20/17
  */
 object ProjectPlugin extends AutoPlugin {
-  override def requires = SiteScaladocPlugin && ParadoxSitePlugin && TutPlugin && GhpagesPlugin
-
   override def trigger: PluginTrigger = allRequirements
 
   import autoImport._
@@ -106,10 +102,9 @@ object ProjectPlugin extends AutoPlugin {
         releaseTagName := s"${version.value}",
         releaseProcess := Seq[ReleaseStep](
           checkSnapshotDependencies,
-          checkGitFlowExists,
           inquireVersions,
+          runClean,
           runTest,
-          gitFlowReleaseStart,
           setReleaseVersion,
           buildSite,
           publishSite,
@@ -140,9 +135,9 @@ object ProjectPlugin extends AutoPlugin {
       apiURL := Some(url("http://rasterframes.io/latest/api")),
       autoAPIMappings := false,
       paradoxProperties in Paradox ++= Map(
-        "github.base_url" -> "https://github.com/s22s/raster-frames",
-        "scaladoc.org.apache.spark.sql.gt" -> "http://rasterframes.io/latest",
-        "scaladoc.geotrellis.base_url" -> "https://geotrellis.github.io/scaladocs/latest"
+        "github.base_url" -> "https://github.com/s22s/raster-frames"//,
+        //"scaladoc.org.apache.spark.sql.gt" -> "http://rasterframes.io/latest" //,
+        //"scaladoc.geotrellis.base_url" -> "https://geotrellis.github.io/scaladocs/latest"
       ),
       sourceDirectory in Paradox := tutTargetDirectory.value,
       sourceDirectory in Paradox in paradoxTheme := sourceDirectory.value / "main" / "paradox" / "_template",
