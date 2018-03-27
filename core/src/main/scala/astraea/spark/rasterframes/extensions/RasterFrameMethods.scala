9e671bd3320bc93dbfac7837823b9f84bbcdb5eb
Simeon H.K. Fitch <fitch@astraea.io>
2018-03-27 16:33:38 -0400

    Added `toMultibandRaster`.
    Signed-off-by: Simeon H.K. Fitch <fitch@astraea.io>

--- core/src/main/scala/astraea/spark/rasterframes/extensions/RasterFrameMethods.scala
+++ core/src/main/scala/astraea/spark/rasterframes/extensions/RasterFrameMethods.scala
@@ -22,7 +22,7 @@ import astraea.spark.rasterframes.util._
 import astraea.spark.rasterframes.{MetadataKeys, RasterFrame}
 import geotrellis.proj4.CRS
 import geotrellis.raster.resample.{Bilinear, ResampleMethod}
-import geotrellis.raster.{MultibandTile, ProjectedRaster, Tile, TileLayout}
+import geotrellis.raster.{CellGrid, MultibandTile, ProjectedRaster, Tile, TileLayout}
 import geotrellis.spark._
 import geotrellis.spark.io._
 import geotrellis.spark.tiling.{LayoutDefinition, Tiler}
@@ -221,10 +221,14 @@ trait RasterFrameMethods extends MethodExtensions[RasterFrame]
 
   /** Convert all the tile columns in a Rasterrame to a GeoTrellis [[MultibandTileLayerRDD]] */
   def toMultibandTileLayerRDD: Either[MultibandTileLayerRDD[SpatialKey], MultibandTileLayerRDD[SpaceTimeKey]] =
+    toMultibandTileLayerRDD(self.tileColumns: _*)
+
+  /** Convert the specified tile columns in a Rasterrame to a GeoTrellis [[MultibandTileLayerRDD]] */
+  def toMultibandTileLayerRDD(tileCols: Column*): Either[MultibandTileLayerRDD[SpatialKey], MultibandTileLayerRDD[SpaceTimeKey]] =
     tileLayerMetadata.fold(
       tlm ⇒ {
         val rdd = self
-          .select(self.spatialKeyColumn, array(self.tileColumns: _*)).as[(SpatialKey, Array[Tile])]
+          .select(self.spatialKeyColumn, array(tileCols: _*)).as[(SpatialKey, Array[Tile])]
           .rdd
           .map { case (sk, tiles) ⇒
             (sk, MultibandTile(tiles))
@@ -233,7 +237,7 @@ trait RasterFrameMethods extends MethodExtensions[RasterFrame]
       },
       tlm ⇒ {
         val rdd = self
-          .select(self.spatialKeyColumn, self.temporalKeyColumn.get, array(self.tileColumns: _*)).as[(SpatialKey, TemporalKey, Array[Tile])]
+          .select(self.spatialKeyColumn, self.temporalKeyColumn.get, array(tileCols: _*)).as[(SpatialKey, TemporalKey, Array[Tile])]
           .rdd
           .map { case (sk, tk, tiles) ⇒ (SpaceTimeKey(sk, tk), MultibandTile(tiles)) }
         Right(ContextRDD(rdd, tlm))
@@ -244,8 +248,50 @@ trait RasterFrameMethods extends MethodExtensions[RasterFrame]
   private[rasterframes] def extract[M: JsonFormat](metadataKey: String)(md: Metadata) =
     md.getMetadata(metadataKey).json.parseJson.convertTo[M]
 
-  /** Convert the tiles in the RasterFrame into a single raster. For RasterFrames keyed with temporal keys, this
-    * will merge based */
+  // TODO: Take care of DRY below
+//  private def rasterize[T <: CellGrid: TypeTag](
+//    tileCols: Seq[Column],
+//    rasterCols: Int,
+//    rasterRows: Int,
+//    resampler: ResampleMethod): ProjectedRaster[T] = {
+//
+//    val clipped = clipLayerExtent
+//
+//    val md = clipped.tileLayerMetadata.widen
+//    val newLayout = LayoutDefinition(md.extent, TileLayout(1, 1, rasterCols, rasterRows))
+//
+//    val trans = md.mapTransform
+//
+//    //val cellType = rdd.first()._2.cellType
+//    val keyBounds = Bounds(SpatialKey(0, 0), SpatialKey(0, 0))
+//    val newLayerMetadata =
+//      md.copy(layout = newLayout, bounds = keyBounds)
+//
+//
+//    val newLayer = typeOf[T] match {
+//      case tpe if tpe <:< typeOf[Tile] ⇒
+//        val r = clipped.toTileLayerRDD(tileCols.head)
+//          .fold(identity, _.map { case (stk, t) ⇒ (stk.spatialKey, t) }) // <-- Drops the temporal key outright
+//          .map { case (key, tile) ⇒ (ProjectedExtent(trans(key), md.crs), tile) }
+//        ContextRDD(r, md)
+//          .tileToLayout(newLayerMetadata, Tiler.Options(resampler))
+//      case tpe if tpe <:< typeOf[MultibandTile] ⇒
+//        val r = clipped.toMultibandTileLayerRDD(tileCols: _*)
+//          .fold(identity, _.map { case (stk, t) ⇒ (stk.spatialKey, t) }) // <-- Drops the temporal key outright
+//          .map { case (key, tile) ⇒ (ProjectedExtent(trans(key), md.crs), tile) }
+//        ContextRDD(r, md)
+//          .tileToLayout(newLayerMetadata, Tiler.Options(resampler))
+//    }
+//
+//    val stitchedTile = newLayer.stitch()
+//
+//    val croppedTile = stitchedTile.crop(rasterCols, rasterRows)
+//
+//    ProjectedRaster(croppedTile, md.extent, md.crs)
+//  }
+
+  /** Convert the tiles in the RasterFrame into a single raster. For RasterFrames keyed with temporal keys, they
+    * will be merge undeterministically. */
   def toRaster(tileCol: Column,
                rasterCols: Int,
                rasterRows: Int,
@@ -255,10 +301,10 @@ trait RasterFrameMethods extends MethodExtensions[RasterFrame]
 
     val md = clipped.tileLayerMetadata.widen
     val trans = md.mapTransform
-    val keyCol = clipped.spatialKeyColumn
     val newLayout = LayoutDefinition(md.extent, TileLayout(1, 1, rasterCols, rasterRows))
 
-    val rdd: RDD[(SpatialKey, Tile)] = clipped.select(keyCol, tileCol).as[(SpatialKey, Tile)].rdd
+    val rdd = clipped.toTileLayerRDD(tileCol)
+      .fold(identity, _.map{ case(stk, t) ⇒ (stk.spatialKey, t) }) // <-- Drops the temporal key outright
 
     val cellType = rdd.first()._2.cellType
 
@@ -279,4 +325,39 @@ trait RasterFrameMethods extends MethodExtensions[RasterFrame]
     ProjectedRaster(croppedTile, md.extent, md.crs)
   }
 
+  /** Convert the Red, Green & Blue assigned tiles in the RasterFrame into a single color composite raster.
+   * For RasterFrames keyed with temporal keys, they will be merged underterministically. */
+  def toMultibandRaster(
+    tileCols: Seq[Column],
+    rasterCols: Int,
+    rasterRows: Int,
+    resampler: ResampleMethod = Bilinear): ProjectedRaster[MultibandTile] = {
+
+    val clipped = clipLayerExtent
+
+    val md = clipped.tileLayerMetadata.widen
+    val trans = md.mapTransform
+    val newLayout = LayoutDefinition(md.extent, TileLayout(1, 1, rasterCols, rasterRows))
+
+    val rdd = clipped.toMultibandTileLayerRDD(tileCols: _*)
+      .fold(identity, _.map{ case(stk, t) ⇒ (stk.spatialKey, t)}) // <-- Drops the temporal key outright
+
+    val cellType = rdd.first()._2.cellType
+
+    val newLayerMetadata =
+      md.copy(layout = newLayout, bounds = Bounds(SpatialKey(0, 0), SpatialKey(0, 0)), cellType = cellType)
+
+    val newLayer = rdd
+      .map {
+        case (key, tile) ⇒
+          (ProjectedExtent(trans(key), md.crs), tile)
+      }
+      .tileToLayout(newLayerMetadata, Tiler.Options(resampler))
+
+    val stitchedTile = newLayer.stitch()
+
+    val croppedTile = stitchedTile.crop(rasterCols, rasterRows)
+
+    ProjectedRaster(croppedTile, md.extent, md.crs)
+  }
 }
